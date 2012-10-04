7cd8768dff8e8e6dd715ebdf94242c03c76a6878
Gabriel Grant <g@briel.ca>
2012-10-04 08:59:13 -0700

    add mising MANIFEST to include README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pro',
-    version='0.1.1',
+    version='0.1.2',
     author='Gabriel Grant',
     author_email='g@briel.ca',
     py_modules=['pro'],
